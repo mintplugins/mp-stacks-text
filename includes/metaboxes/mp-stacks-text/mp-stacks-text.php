1d329ecabdc69622a8d3f57ab4899b85eeef562e
Phil Johnston <johnstonphilip@gmail.com>
2013-05-08 23:33:03 -0400

    beta1.0.0.2
    bricks update
    
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/metaboxes/mp-stacks-text/mp-stacks-text.php
+++ includes/metaboxes/mp-stacks-text/mp-stacks-text.php
@@ -6,42 +6,42 @@
 function mp_stacks_text_additional_items_array($items_array) {
     array_push($items_array,
        array(
-			'field_id'	 => 'stack_title_color',
+			'field_id'	 => 'brick_title_color',
 			'field_title' => __( 'Title Color (Optional)', 'mp_stacks'),
 			'field_description' => 'Select the color the title of this stack.',
 			'field_type' => 'colorpicker',
 			'field_value' => ''
 		),
 		array(
-			'field_id'	 => 'stack_title_font_size',
+			'field_id'	 => 'brick_title_font_size',
 			'field_title' => __( 'Title Font Size (Optional)', 'mp_stacks'),
 			'field_description' => 'Enter the size for the title (in pixels).',
 			'field_type' => 'number',
 			'field_value' => ''
 		),
 		array(
-			'field_id'	 => 'stack_title_font_css',
+			'field_id'	 => 'brick_title_font_css',
 			'field_title' => __( 'Title Font CSS (Optional)', 'mp_stacks'),
 			'field_description' => 'Enter some styles for the title.',
 			'field_type' => 'textarea',
 			'field_value' => ''
 		),
 		array(
-			'field_id'	 => 'stack_text_color',
+			'field_id'	 => 'brick_text_color',
 			'field_title' => __( 'Text Color (Optional)', 'mp_stacks'),
 			'field_description' => 'Select the color the text of this stack.',
 			'field_type' => 'colorpicker',
 			'field_value' => ''
 		),
 		array(
-			'field_id'	 => 'stack_text_font_size',
+			'field_id'	 => 'brick_text_font_size',
 			'field_title' => __( 'Text Font Size (Optional)', 'mp_stacks'),
 			'field_description' => 'Enter the size for the title (in pixels).',
 			'field_type' => 'number',
 			'field_value' => ''
 		),
 		array(
-			'field_id'	 => 'stack_text_font_css',
+			'field_id'	 => 'brick_text_font_css',
 			'field_title' => __( 'Text Font CSS (Optional)', 'mp_stacks'),
 			'field_description' => 'Enter some styles for the text.',
 			'field_type' => 'textarea',
--- includes/plugin-checker/included-plugins/mp-core-check.php
+++ includes/plugin-checker/included-plugins/mp-core-check.php
@@ -7,7 +7,7 @@
 	function mp_core_plugin_check() {
 		$args = array(
 			'plugin_name' => 'Move Plugins Core',  //<--Make sure this matches the name of the plugin and the name of the plugin in mp_repo
-			'plugin_message' => __('You require the Move Plugins Core plugin. Install it here.', 'mp_links'), 
+			'plugin_message' => __('You require the Move Plugins Core plugin. Install it here.', 'mp_stacks_text'), 
 			'plugin_slug' => 'mp-core', //<--This will be the name of the subdirectory where the plugin is located
 			'plugin_filename' => 'mp-core.php',
 			'plugin_required' => true,
