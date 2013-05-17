b76089ac4ba244ac9781ba613f2f42748ba91421
Phil Johnston <johnstonphilip@gmail.com>
2013-05-17 16:52:55 -0400

    repo update
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/plugin-checker/included-plugins/mp-stacks.php
+++ includes/plugin-checker/included-plugins/mp-stacks.php
@@ -11,7 +11,7 @@
 			'plugin_slug' => 'mp-stacks', 
 			'plugin_filename' => 'mp-stacks.php',
 			'plugin_required' => true,
-			'plugin_download_link' => 'http://moveplugins.com/repo/mp-stacks/?download=true'
+			'plugin_download_link' => 'http://repo.moveplugins.com/repo/mp-stacks/?downloadfile=true'
 		);
 		$mp_core_plugin_check = new MP_CORE_Plugin_Checker($args);
 	}
