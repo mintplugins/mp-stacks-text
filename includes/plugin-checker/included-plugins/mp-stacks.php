2d84d0c25a4126a70ea87061ab3fdb9ec2f13745
Phil Johnston <johnstonphilip@gmail.com>
2013-09-27 15:52:53 -0400

    beta1.0.0.9
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/plugin-checker/included-plugins/mp-stacks.php
+++ includes/plugin-checker/included-plugins/mp-stacks.php
@@ -1,20 +1,42 @@
 <?php
 /**
- * Install mp_stacks Plugin
+ * This file contains a function which checks if the MP Stacks plugin is installed.
  *
+ * @since 1.0.0
+ *
+ * @package    MP Core
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2013, Move Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
  */
- if (!function_exists('mp_stacks_plugin_check')){
-	function mp_stacks_plugin_check() {
-		$args = array(
-			'plugin_name' => 'MP Stacks', 
-			'plugin_message' => __('You require the Move Plugins Stacks plugin. Install it here.', 'mp_stacks_text'), 
-			'plugin_slug' => 'mp-stacks', 
-			'plugin_filename' => 'mp-stacks.php',
-			'plugin_required' => true,
-			'plugin_download_link' => 'http://repo.moveplugins.com/repo/mp-stacks/?downloadfile=true'
+ 
+/**
+* Check to make sure the MP Stacks Plugin is installed.
+*
+* @since    1.0.0
+* @link     http://moveplugins.com/doc/plugin-checker-class/
+* @return   array $plugins An array of plugins to be installed. This is passed in through the mp_core_check_plugins filter.
+* @return   array $plugins An array of plugins to be installed. This is passed to the mp_core_check_plugins filter. (see link).
+*/
+if (!function_exists('mp_stacks_plugin_check')){
+	function mp_stacks_plugin_check( $plugins ) {
+		
+		$add_plugins = array(
+			array(
+				'plugin_name' => 'MP Stacks',
+				'plugin_message' => __('You require the MP Stacks plugin. Install it here.', 'mp_stacks_text'),
+				'plugin_filename' => 'mp-stacks.php',
+				'plugin_download_link' => 'http://moveplugins.com/stacks/mp-stacks/?downloadfile=true',
+				'plugin_info_link' => 'http://moveplugins.com/plugins/mp-stacks',
+				'plugin_group_install' => true,
+				'plugin_required' => true,
+				'plugin_wp_stacks' => true,
+			)
 		);
-		$mp_core_plugin_check = new MP_CORE_Plugin_Checker($args);
+		
+		return array_merge( $plugins, $add_plugins );
 	}
- }
-add_action( 'admin_init', 'mp_stacks_plugin_check' );
-
+}
+add_filter( 'mp_core_check_plugins', 'mp_stacks_plugin_check' );
--- includes/updater/mp-stacks-text-update.php
+++ includes/updater/mp-stacks-text-update.php
@@ -1,13 +1,29 @@
 <?php
 /**
- * Check for updates for this Plugin
+ * This file contains the function keeps the MP Stacks Text plugin up to date.
  *
+ * @since 1.0.0
+ *
+ * @package    MP Stacks Text
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2013, Move Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
+ */
+ 
+/**
+ * Check for updates for the MP Stacks Text Plugin by creating a new instance of the MP_CORE_Plugin_Updater class.
+ *
+ * @access   public
+ * @since    1.0.0
+ * @return   void
  */
  if (!function_exists('mp_stacks_text_update')){
 	function mp_stacks_text_update() {
 		$args = array(
-			'software_name' => 'MP Stacks Text Addon', //<- The exact name of this Plugin. Make sure it matches the title in your mp_stacks_text, edd, and the WP.org repo
-			'software_api_url' => 'http://repo.moveplugins.com',//The URL where EDD and mp_stacks_text are installed and checked
+			'software_name' => 'MP Stacks Text', //<- The exact name of this Plugin. Make sure it matches the title in your mp_stacks-text, text, and the WP.org stacks-text
+			'software_api_url' => 'http://moveplugins.com',//The URL where Text and mp_stacks-text are installed and checked
 			'software_filename' => 'mp-stacks-text.php',
 			'software_licensed' => false, //<-Boolean
 		);
